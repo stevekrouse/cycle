9a1b5bb44c7fb542747af68a3399ef3ea797bcb4
Steve Krouse <skrouse@seas.upenn.edu>
2017-01-20 20:43:21 +0000

    fixed typing bug, made eval faster, input text scaffolding

--- generator.js
+++ generator.js
@@ -55,125 +55,81 @@ Blockly.JavaScript.lists_indexOf = function(a) {
     return [(Blockly.JavaScript.valueToCode(a, "VALUE", Blockly.JavaScript.ORDER_MEMBER) || "[]") + ".map(function(elem) { return JSON.stringify(elem) })." + b + "(JSON.stringify(" + c + ")) + 1", Blockly.JavaScript.ORDER_MEMBER]
 };
 
-Blockly.JavaScript.lists_setIndex = function(a) {
-    function b() {
-        if (c.match(/^\w+$/)) return "";
-        var a = Blockly.JavaScript.variableDB_.getDistinctName("tmp_list", Blockly.Variables.NAME_TYPE),
-            b = "var " + a + " = " + c + ";\n";
-        c = a;
-        return b
+Blockly.JavaScript['lists_setIndex'] = function(block) {
+  // Set element at index.
+  // Note: Until February 2013 this block did not have MODE or WHERE inputs.
+  var list = Blockly.JavaScript.valueToCode(block, 'LIST',
+      Blockly.JavaScript.ORDER_MEMBER) || '[]';
+  var mode = block.getFieldValue('MODE') || 'GET';
+  var where = block.getFieldValue('WHERE') || 'FROM_START';
+  var value = Blockly.JavaScript.valueToCode(block, 'TO',
+      Blockly.JavaScript.ORDER_ASSIGNMENT) || 'null';
+  // Cache non-trivial values to variables to prevent repeated look-ups.
+  // Closure, which accesses and modifies 'list'.
+  function cacheList() {
+    if (list.match(/^\w+$/)) {
+      return '';
     }
-    var c = Blockly.JavaScript.valueToCode(a, "LIST", Blockly.JavaScript.ORDER_MEMBER) || "[]",
-        d = a.getFieldValue("MODE") || "GET",
-        e = a.getFieldValue("WHERE") || "FROM_START",
-        f = Blockly.JavaScript.valueToCode(a, "AT", Blockly.JavaScript.ORDER_NONE) || "1";
-    a = Blockly.JavaScript.valueToCode(a, "TO", Blockly.JavaScript.ORDER_ASSIGNMENT) ||
-        "null";
-    if ("FIRST" == e) {
-        if ("SET" == d) return "Vue.set(" + c + ", 0, " + a + ');\n';
-        if ("INSERT" == d) return c + ".unshift(" + a + ");\n"
-    }
-    else if ("LAST" == e) {
-        if ("SET" == d) return e = b(), e + ("Vue.set(" + c + ", " + c + '.length - 1' + ", " + a + ');\n');
-        if ("INSERT" == d) return c + ".push(" + a + ");\n"
-    }
-    else if ("FROM_START" == e) {
-        f = Blockly.isNumber(f) ? parseFloat(f) - 1 : f + " - 1";
-        if ("SET" == d) return "Vue.set(" + c + ", " + f + ", " + a + ');\n';
-        if ("INSERT" == d) return c + ".splice(" + f + ", 0, " + a + ");\n"
-    }
-    else if ("FROM_END" == e) {
-        e = b();
-        if ("SET" == d) return e += "Vue.set(" + c + ", " + c + '.length - ' + f + ", " + a + ');\n';
-        if ("INSERT" == d) return e +=
-            c + ".splice(" + c + ".length - " + f + ", 0, " + a + ");\n"
-    }
-    else if ("RANDOM" == e) {
-        e = b();
-        f = Blockly.JavaScript.variableDB_.getDistinctName("tmp_x", Blockly.Variables.NAME_TYPE);
-        e += "var " + f + " = Math.floor(Math.random() * " + c + ".length);\n";
-        if ("SET" == d) return e += "Vue.set(" + c + ", " + f + ", " + a + ');\n';
-        if ("INSERT" == d) return e += c + ".splice(" + f + ", 0, " + a + ");\n"
-    }
-    throw "Unhandled combination (lists_setIndex).";
+    var listVar = Blockly.JavaScript.variableDB_.getDistinctName(
+        'tmpList', Blockly.Variables.NAME_TYPE);
+    var code = 'var ' + listVar + ' = ' + list + ';\n';
+    list = listVar;
+    return code;
+  }
+  switch (where) {
+    case ('FIRST'):
+      if (mode == 'SET') {
+        return list + '[0] = ' + value + ';\n';
+      } else if (mode == 'INSERT') {
+        return list + '.unshift(' + value + ');\n';
+      }
+      break;
+    case ('LAST'):
+      if (mode == 'SET') {
+        var code = cacheList();
+        code += "Vue.set(" + list + ", " + list + '.length - 1' + ", " + value + ');\n';
+        return code;
+      } else if (mode == 'INSERT') {
+        return list + '.push(' + value + ');\n';
+      }
+      break;
+    case ('FROM_START'):
+      var at = Blockly.JavaScript.getAdjusted(block, 'AT');
+      if (mode == 'SET') {
+        return "Vue.set(" + list + ", " + at + ", " + value + ');\n';
+      } else if (mode == 'INSERT') {
+        return list + '.splice(' + at + ', 0, ' + value + ');\n';
+      }
+      break;
+    case ('FROM_END'):
+      var at = Blockly.JavaScript.getAdjusted(block, 'AT', 1, false,
+          Blockly.JavaScript.ORDER_SUBTRACTION);
+      var code = cacheList();
+      if (mode == 'SET') {
+        code += "Vue.set(" + list + ", " + list + '.length - ' + at + ", " + value + ');\n';
+        return code;
+      } else if (mode == 'INSERT') {
+        code += list + '.splice(' + list + '.length - ' + at + ', 0, ' + value + ');\n';
+        return code;
+      }
+      break;
+    case ('RANDOM'):
+      var code = cacheList();
+      var xVar = Blockly.JavaScript.variableDB_.getDistinctName(
+          'tmpX', Blockly.Variables.NAME_TYPE);
+      code += 'var ' + xVar + ' = Math.floor(Math.random() * ' + list +
+          '.length);\n';
+      if (mode == 'SET') {
+        code += "Vue.set(" + list + ", " + xVar + ", " + value + ');\n';
+        return code;
+      } else if (mode == 'INSERT') {
+        code += list + '.splice(' + xVar + ', 0, ' + value + ');\n';
+        return code;
+      }
+      break;
+  }
+  throw 'Unhandled combination (lists_setIndex).';
 };
-
-// eventually when I updated blockly
-// Blockly.JavaScript['lists_setIndex'] = function(block) {
-//   // Set element at index.
-//   // Note: Until February 2013 this block did not have MODE or WHERE inputs.
-//   var list = Blockly.JavaScript.valueToCode(block, 'LIST',
-//       Blockly.JavaScript.ORDER_MEMBER) || '[]';
-//   var mode = block.getFieldValue('MODE') || 'GET';
-//   var where = block.getFieldValue('WHERE') || 'FROM_START';
-//   var value = Blockly.JavaScript.valueToCode(block, 'TO',
-//       Blockly.JavaScript.ORDER_ASSIGNMENT) || 'null';
-//   // Cache non-trivial values to variables to prevent repeated look-ups.
-//   // Closure, which accesses and modifies 'list'.
-//   function cacheList() {
-//     if (list.match(/^\w+$/)) {
-//       return '';
-//     }
-//     var listVar = Blockly.JavaScript.variableDB_.getDistinctName(
-//         'tmpList', Blockly.Variables.NAME_TYPE);
-//     var code = 'var ' + listVar + ' = ' + list + ';\n';
-//     list = listVar;
-//     return code;
-//   }
-//   switch (where) {
-//     case ('FIRST'):
-//       if (mode == 'SET') {
-//         return list + '[0] = ' + value + ';\n';
-//       } else if (mode == 'INSERT') {
-//         return list + '.unshift(' + value + ');\n';
-//       }
-//       break;
-//     case ('LAST'):
-//       if (mode == 'SET') {
-//         var code = cacheList();
-//         code += "Vue.set(" + list + ", " + list + '.length - 1' + ", " + value + ');\n';
-//         return code;
-//       } else if (mode == 'INSERT') {
-//         return list + '.push(' + value + ');\n';
-//       }
-//       break;
-//     case ('FROM_START'):
-//       var at = Blockly.JavaScript.getAdjusted(block, 'AT');
-//       if (mode == 'SET') {
-//         return "Vue.set(" + list + ", " + at + ", " + value + ');\n';
-//       } else if (mode == 'INSERT') {
-//         return list + '.splice(' + at + ', 0, ' + value + ');\n';
-//       }
-//       break;
-//     case ('FROM_END'):
-//       var at = Blockly.JavaScript.getAdjusted(block, 'AT', 1, false,
-//           Blockly.JavaScript.ORDER_SUBTRACTION);
-//       var code = cacheList();
-//       if (mode == 'SET') {
-//         code += "Vue.set(" + list + ", " + list + '.length - ' + at + ", " + value + ');\n';
-//         return code;
-//       } else if (mode == 'INSERT') {
-//         code += list + '.splice(' + list + '.length - ' + at + ', 0, ' + value + ');\n';
-//         return code;
-//       }
-//       break;
-//     case ('RANDOM'):
-//       var code = cacheList();
-//       var xVar = Blockly.JavaScript.variableDB_.getDistinctName(
-//           'tmpX', Blockly.Variables.NAME_TYPE);
-//       code += 'var ' + xVar + ' = Math.floor(Math.random() * ' + list +
-//           '.length);\n';
-//       if (mode == 'SET') {
-//         code += "Vue.set(" + list + ", " + xVar + ", " + value + ');\n';
-//         return code;
-//       } else if (mode == 'INSERT') {
-//         code += list + '.splice(' + xVar + ', 0, ' + value + ');\n';
-//         return code;
-//       }
-//       break;
-//   }
-//   throw 'Unhandled combination (lists_setIndex).';
-// };
 
 Blockly.JavaScript['cycle_emit'] = function(block) {
   var value_name = Blockly.JavaScript.valueToCode(block, 'NAME', Blockly.JavaScript.ORDER_ATOMIC);
